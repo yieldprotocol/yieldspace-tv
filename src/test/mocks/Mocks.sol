958240fcaef4bbf9d9fab092b8bd7a7f036b6e21
Alberto Cuesta Cañada <alberto@yield.is>
2022-07-15 15:56:05 +0100

    forge install: vault-v2
    feat/deprecate-vault-interfaces

--- src/test/mocks/Mocks.sol
+++ src/test/mocks/Mocks.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
-import "@yield-protocol/vault-interfaces/src/DataTypes.sol";
+import "@yield-protocol/vault-v2/contracts/interfaces/DataTypes.sol";
 
 //common utilities for forge tests
 library Mocks  {
