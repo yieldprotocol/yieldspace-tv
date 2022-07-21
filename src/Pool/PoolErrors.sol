bca5824031262332575656e102014d8e590877b8
Richie <devtooligan@gmail.com>
2022-07-20 18:47:25 -0700

    fix: check for fytokencache < totalsupply

--- src/Pool/PoolErrors.sol
+++ src/Pool/PoolErrors.sol
@@ -10,6 +10,11 @@ error AfterMaturity();
 /// The approval of the sharesToken failed miserably.
 error ApproveFailed();
 
+/// The update would cause the FYToken cached to be less than the total supply. This should never happen but may
+/// occur due to unexpected rounding errors.  We cannot allow this to happen as it could have many unexpected and
+/// side effects which may pierce the fabric of the space-time continuum.
+error FYTokenCachedBadState();
+
 /// The pool has already been initialized. What are you thinking?
 /// @dev To save gas, total supply == 0 is checked instead of a state variable.
 error Initialized();
