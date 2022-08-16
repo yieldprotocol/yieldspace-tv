585a0db8beef69baac1c765e5bcf76c552521c57
Richie <devtooligan@gmail.com>
2022-08-16 13:14:29 -0700

    feat: hoagies

--- src/YieldMathExtensions.sol
+++ src/YieldMathExtensions.sol
@@ -8,10 +8,10 @@ import "./YieldMath.sol";
 library YieldMathExtensions {
 
     /// @dev Calculate the invariant for this pool
-    function invariant(IPool pool) external view returns (uint128) {
+    function hoagies(IPool pool) external view returns (uint128) {
         uint32 maturity = pool.maturity();
         uint32 timeToMaturity = (maturity > uint32(block.timestamp)) ? maturity - uint32(block.timestamp) : 0;
-        return YieldMath.invariant(
+        return YieldMath.hoagies(
             pool.getBaseBalance(),
             pool.getFYTokenBalance(),
             pool.totalSupply(),
