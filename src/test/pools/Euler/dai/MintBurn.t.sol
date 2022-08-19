09c1aa54104dbf5ceca169c9596795ace1d1fe5d
marcomariscal <marco.a.mariscal@gmail.com>
2022-08-19 10:35:11 -0700

    feat: new Euler folder structure to parse dai/usdc into separate folders with mintBurn, trading, and state individual files

---
+++ src/test/pools/Euler/dai/MintBurn.t.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.15;
+
+/*
+  __     ___      _     _
+  \ \   / (_)    | |   | | ████████╗███████╗███████╗████████╗███████╗
+   \ \_/ / _  ___| | __| | ╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██╔════╝
+    \   / | |/ _ \ |/ _` |    ██║   █████╗  ███████╗   ██║   ███████╗
+     | |  | |  __/ | (_| |    ██║   ██╔══╝  ╚════██║   ██║   ╚════██║
+     |_|  |_|\___|_|\__,_|    ██║   ███████╗███████║   ██║   ███████║
+      yieldprotocol.com       ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝
+
+*/
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//    NOTE:
+//    These tests are exactly copy and pasted from the MintBurn.t.sol and TradingUSDC.t.sol test suites.
+//    The only difference is they are setup on the PoolEuler contract instead of the Pool contract
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {console} from "forge-std/console.sol";
+
+import "../../../../Pool/PoolErrors.sol";
+import {Exp64x64} from "../../../../Exp64x64.sol";
+import {Math64x64} from "../../../../Math64x64.sol";
+import {YieldMath} from "../../../../YieldMath.sol";
+import {CastU256U128} from "@yield-protocol/utils-v2/contracts/cast/CastU256U128.sol";
+
+import "../../../shared/Utils.sol";
+import "../../../shared/Constants.sol";
+import {ETokenMock} from "../../../mocks/ETokenMock.sol";
+import {ZeroState, ZeroStateParams} from "../../../shared/ZeroState.sol";
+import {SyncablePoolEuler} from "../../../mocks/SyncablePoolEuler.sol";
+import {IERC20Like} from "../../../../interfaces/IERC20Like.sol";
+import "./State.sol";
+
+contract AdminDAI__WithLiquidityEuler is WithLiquidityEulerDAI {
+    function testUnit_admin1_EulerDAI() public {
+        console.log("retrieveBase returns nothing if there is no excess");
+        uint256 startingBaseBalance = pool.baseToken().balanceOf(alice);
+        uint256 startingSharesBalance = pool.sharesToken().balanceOf(alice);
+        (uint104 startingSharesCached, uint104 startingFyTokenCached, , ) = pool.getCache();
+
+        pool.retrieveBase(alice);
+
+        (uint104 currentSharesCached, uint104 currentFyTokenCached, , ) = pool.getCache();
+        assertEq(currentSharesCached, startingSharesCached);
+        assertEq(currentFyTokenCached, startingFyTokenCached);
+        assertEq(pool.baseToken().balanceOf(alice), startingBaseBalance);
+        assertEq(pool.sharesToken().balanceOf(alice), startingSharesBalance);
+    }
+
+    function testUnit_admin2_EulerDAI() public {
+        console.log("retrieveBase returns exceess");
+        uint256 additionalAmount = 69;
+        IERC20Like base = IERC20Like(address(pool.baseToken()));
+        vm.prank(alice);
+        base.transfer(address(pool), additionalAmount);
+
+        uint256 startingBaseBalance = pool.baseToken().balanceOf(alice);
+        uint256 startingSharesBalance = pool.sharesToken().balanceOf(alice);
+        (uint104 startingSharesCached, uint104 startingFyTokenCached, , ) = pool.getCache();
+
+        pool.retrieveBase(alice);
+
+        (uint104 currentSharesCached, uint104 currentFyTokenCached, , ) = pool.getCache();
+        assertEq(currentSharesCached, startingSharesCached);
+        assertEq(currentFyTokenCached, startingFyTokenCached);
+        assertEq(pool.baseToken().balanceOf(alice), startingBaseBalance + additionalAmount);
+        assertEq(pool.sharesToken().balanceOf(alice), startingSharesBalance);
+    }
+
+    function testUnit_admin3_EulerDAI() public {
+        console.log("retrieveShares returns nothing if there is no excess");
+        uint256 startingBaseBalance = pool.baseToken().balanceOf(alice);
+        uint256 startingSharesBalance = pool.sharesToken().balanceOf(alice);
+        (uint104 startingSharesCached, uint104 startingFyTokenCached, , ) = pool.getCache();
+
+        pool.retrieveShares(alice);
+
+        // There is a 1 wei difference attributable to some deep nested rounding
+        assertApproxEqAbs(pool.baseToken().balanceOf(alice), startingBaseBalance, 1);
+        // assertEq(pool.sharesToken().balanceOf(alice), startingSharesBalance);
+        (uint104 currentSharesCached, uint104 currentFyTokenCached, , ) = pool.getCache();
+        assertEq(currentFyTokenCached, startingFyTokenCached);
+    }
+
+    function testUnit_admin4_EulerDAI() public {
+        console.log("retrieveShares returns exceess");
+
+        (uint104 startingSharesCached, uint104 startingFyTokenCached, , ) = pool.getCache();
+
+        uint256 additionalAmount = 69e18;
+        shares.mint(address(pool), additionalAmount);
+
+        uint256 startingBaseBalance = pool.baseToken().balanceOf(alice);
+        uint256 startingSharesBalance = pool.sharesToken().balanceOf(alice);
+
+        pool.retrieveShares(alice);
+
+        (uint104 currentSharesCached, uint104 currentFyTokenCached, , ) = pool.getCache();
+        assertEq(currentFyTokenCached, startingFyTokenCached);
+        assertEq(currentSharesCached, startingSharesCached);
+        assertEq(pool.baseToken().balanceOf(alice), startingBaseBalance);
+
+        // There is a 1 wei difference attributable to some deep nested rounding
+        assertApproxEqAbs(pool.sharesToken().balanceOf(alice), startingSharesBalance + additionalAmount, 1);
+    }
+
+    function testUnit_admin5_EulerDAI() public {
+        console.log("retrieveFYToken returns nothing if there is no excess");
+        uint256 startingBaseBalance = pool.baseToken().balanceOf(alice);
+        uint256 startingSharesBalance = pool.sharesToken().balanceOf(alice);
+        uint256 startingFyTokenBalance = pool.fyToken().balanceOf(alice);
+        (uint104 startingSharesCached, uint104 startingFyTokenCached, , ) = pool.getCache();
+
+        pool.retrieveFYToken(alice);
+
+        assertEq(pool.baseToken().balanceOf(alice), startingBaseBalance);
+        assertEq(pool.sharesToken().balanceOf(alice), startingSharesBalance);
+        assertEq(pool.fyToken().balanceOf(alice), startingFyTokenBalance);
+        (uint104 currentSharesCached, uint104 currentFyTokenCached, , ) = pool.getCache();
+        assertEq(currentFyTokenCached, startingFyTokenCached);
+    }
+
+    function testUnit_admin6_EulerDAI() public {
+        console.log("retrieveFYToken returns exceess");
+        uint256 additionalAmount = 69e18;
+        fyToken.mint(address(pool), additionalAmount);
+
+        uint256 startingBaseBalance = pool.baseToken().balanceOf(alice);
+        uint256 startingSharesBalance = pool.sharesToken().balanceOf(alice);
+        uint256 startingFyTokenBalance = pool.fyToken().balanceOf(alice);
+        (uint104 startingSharesCached, uint104 startingFyTokenCached, , ) = pool.getCache();
+
+        pool.retrieveFYToken(alice);
+
+        (uint104 currentSharesCached, uint104 currentFyTokenCached, , ) = pool.getCache();
+        assertEq(currentFyTokenCached, startingFyTokenCached);
+        assertEq(currentSharesCached, startingSharesCached);
+        assertEq(pool.fyToken().balanceOf(alice), startingFyTokenBalance + additionalAmount);
+        assertEq(pool.sharesToken().balanceOf(alice), startingSharesBalance);
+        assertEq(pool.baseToken().balanceOf(alice), startingBaseBalance);
+    }
+}
+
+contract MintWithBase__ZeroStateEulerDAI is ZeroStateEulerDAI {
+    function testUnit_Euler_mintWithBaseDAI01() public {
+        console.log("does not mintWithBase when pool is not initialized");
+
+        vm.expectRevert(NotInitialized.selector);
+        vm.prank(alice);
+        pool.mintWithBase(alice, alice, 0, 0, uint128(MAX));
+    }
+}
+
+contract MintWithBase__WithLiquidityEulerDAI is WithLiquidityEulerDAI {
+    function testUnit_Euler_mintWithBaseDAI02() public {
+        console.log("does not mintWithBase when mature");
+
+        vm.warp(pool.maturity());
+        vm.expectRevert(AfterMaturity.selector);
+        vm.prank(alice);
+        pool.mintWithBase(alice, alice, 0, 0, uint128(MAX));
+    }
+
+    function testUnit_Euler_mintWithBaseDAI03() public {
+        console.log("mints with only base (asset)");
+
+        uint256 assetBalBefore = asset.balanceOf(alice);
+        uint256 fyTokenBalBefore = fyToken.balanceOf(alice);
+        uint256 poolBalBefore = pool.balanceOf(alice);
+
+        // estimate how many shares need to be sold using arbitrary fyTokenToBuy amount and estimate lp tokens minted,
+        // to be able to calculate how much asset to send to the pool
+        uint128 fyTokenToBuy = uint128(1000 * 10**fyToken.decimals());
+        uint128 assetsToSell = pool.buyFYTokenPreview(fyTokenToBuy);
+        uint256 sharesToSell = pool.wrapPreview(assetsToSell);
+        (uint104 sharesReservesBefore, uint104 fyTokenReservesBefore, , ) = pool.getCache();
+        uint256 realFyTokenReserves = fyTokenReservesBefore - pool.totalSupply();
+
+        uint256 fyTokenIn = fyToken.balanceOf(address(pool)) - realFyTokenReserves;
+        // lpTokensMinted = totalSupply * (fyTokenToBuy + fyTokenIn) / realFyTokenReserves - fyTokenToBuy
+        uint256 lpTokensMinted = (pool.totalSupply() * (fyTokenToBuy + fyTokenIn)) /
+            (realFyTokenReserves - fyTokenToBuy);
+
+        uint256 sharesIn = sharesToSell + ((sharesReservesBefore + sharesToSell) * lpTokensMinted) / pool.totalSupply();
+        uint256 assetsIn = pool.unwrapPreview(sharesIn);
+
+        // mintWithBase
+        vm.startPrank(alice);
+        asset.transfer(address(pool), assetsIn);
+        pool.mintWithBase(alice, alice, fyTokenToBuy, 0, uint128(MAX));
+
+        // check user balances
+        assertEq(assetBalBefore - asset.balanceOf(alice), assetsIn);
+        assertEq(fyTokenBalBefore, fyToken.balanceOf(alice));
+        assertEq(pool.balanceOf(alice) - poolBalBefore, lpTokensMinted);
+
+        // check pool reserves
+        (uint104 sharesReservesAfter, uint104 fyTokenReservesAfter, , ) = pool.getCache();
+        assertEq(sharesReservesAfter, pool.getSharesBalance());
+        assertEq(sharesReservesAfter - sharesReservesBefore, sharesIn);
+        assertEq(fyTokenReservesAfter, pool.getFYTokenBalance());
+        assertEq(fyTokenReservesAfter - fyTokenReservesBefore, lpTokensMinted);
+    }
+}
+
+contract BurnForBase__WithLiquidityEulerDAI is WithLiquidityEulerDAI {
+    using Math64x64 for uint256;
+    using CastU256U128 for uint256;
+
+    function testUnit_Euler_burnForBaseDAI01() public {
+        console.log("does not burnForBase when mature");
+
+        vm.warp(pool.maturity());
+        vm.expectRevert(AfterMaturity.selector);
+        vm.prank(alice);
+        pool.burnForBase(alice, 0, uint128(MAX));
+    }
+
+    function testUnit_Euler_burnForBaseDAI02() public {
+        console.log("burns for only base (asset)");
+
+        // using a value that we assume will be below maxSharesOut and maxFYTokenOut, and will allow for trading to base
+        uint256 lpTokensToBurn = 1000 * 10**asset.decimals(); // using the asset decimals, since they match the pool
+
+        uint256 assetBalBefore = asset.balanceOf(alice);
+        uint256 fyTokenBalBefore = fyToken.balanceOf(alice);
+        uint256 poolBalBefore = pool.balanceOf(alice);
+        (uint104 sharesReservesBefore, uint104 fyTokenReservesBefore, , ) = pool.getCache();
+
+        // estimate how many shares and fyToken we will get back from burn
+        uint256 sharesOut = (lpTokensToBurn * sharesReservesBefore) / pool.totalSupply();
+        // fyTokenOut = lpTokensBurned * realFyTokenReserves / totalSupply
+        uint256 fyTokenOut = (lpTokensToBurn * (fyTokenReservesBefore - pool.totalSupply())) / pool.totalSupply();
+
+        // estimate how much shares (and base) we can trade fyToken for, using the new pool state
+        uint256 fyTokenOutToShares = YieldMath.sharesOutForFYTokenIn(
+            (sharesReservesBefore - sharesOut).u128(),
+            (fyTokenReservesBefore - fyTokenOut).u128(),
+            fyTokenOut.u128(),
+            maturity - uint32(block.timestamp),
+            k,
+            pool.g2(),
+            pool.getC(),
+            pool.mu()
+        );
+        uint256 totalSharesOut = sharesOut + fyTokenOutToShares;
+        uint256 expectedAssetsOut = pool.unwrapPreview(totalSharesOut);
+
+        // burnForBase
+        vm.startPrank(alice);
+        pool.transfer(address(pool), lpTokensToBurn);
+        pool.burnForBase(alice, 0, uint128(MAX));
+
+        // check user balances
+        assertApproxEqAbs(asset.balanceOf(alice) - assetBalBefore, expectedAssetsOut, 1);
+        assertEq(fyTokenBalBefore, fyToken.balanceOf(alice));
+        assertEq(poolBalBefore - pool.balanceOf(alice), lpTokensToBurn);
+
+        // check pool reserves
+        (uint104 sharesReservesAfter, uint104 fyTokenReservesAfter, , ) = pool.getCache();
+        assertEq(sharesReservesAfter, pool.getSharesBalance());
+        assertApproxEqAbs(sharesReservesBefore - sharesReservesAfter, totalSharesOut, 1);
+        assertEq(fyTokenReservesAfter, pool.getFYTokenBalance());
+        assertEq(fyTokenReservesBefore - fyTokenReservesAfter, lpTokensToBurn);
+    }
+}
