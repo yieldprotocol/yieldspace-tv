fb922bb90d8f03d68623b9a6ccc2c5370e004668
marcomariscal <marco.a.mariscal@gmail.com>
2022-08-25 13:34:29 -0700

    fix: space

--- src/test/pools/Euler/fork/usdc/State.sol
+++ src/test/pools/Euler/fork/usdc/State.sol
@@ -43,7 +43,7 @@ abstract contract EulerUSDCFork is ForkTestCore {
     address public whale = address(0xbd50C26f7ed3dE3f642149D487f4308a42763bd6);
     IEToken eToken;
     uint8 decimals;
-    uint256 WAD; //scaled to asset decimals
+    uint256 WAD; // scaled to asset decimals
 
     function fundAddr(address addr) public {
         vm.prank(whale);
