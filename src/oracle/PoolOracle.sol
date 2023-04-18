8a0ebc7d40c92382fc998e278407eba65318436b
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2023-04-18 18:20:52 +0200

    add contango audit pdf

--- src/oracle/PoolOracle.sol
+++ src/oracle/PoolOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Audited by [ABDK](https://www.abdk.consulting/) 13/03/23, report pending.
+// Audited by [ABDK](https://www.abdk.consulting/) 13/03/23, [report](audit/ABDK_Contango_Core_v_1_0.pdf).
 pragma solidity >=0.8.15;
 
 import "../interfaces/IPoolOracle.sol";
